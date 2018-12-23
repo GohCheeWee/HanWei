45edbd95fc26a365f0e5aaaaa99056e445f58f1c
GohCheeWee <cheeweegoh1997@gmail.com>
2018-12-23 10:55:44 +0800

    lastest 1

--- app/src/main/java/com/jby/hanwei/LoginActivity.java
+++ app/src/main/java/com/jby/hanwei/LoginActivity.java
@@ -28,6 +28,7 @@ import java.util.Map;
 
 import static com.jby.hanwei.url.UrlManager.loginPath;
 //lol/ hahaha
+//wtf eee
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText username, password;
     private Button loginButton, ipAddress;
